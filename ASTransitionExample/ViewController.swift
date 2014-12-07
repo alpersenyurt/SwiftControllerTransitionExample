2d90c7cfcff45dcb206c94a7942ed48f1c582b3c
Alper Senyurt <alpersenyurt@Alpers-MacBook-Pro.local>
2014-12-07 22:42:44 +0200

    Initial Commit

---
+++ ASTransitionExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ASTransitionExample
+//
+//  Created by Alper Senyurt on 12/7/14.
+//  Copyright (c) 2014 Alper Senyurt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
